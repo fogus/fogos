0c74d3b74f85dab06335f8a104527abbc1bb51fc
fogus <mefogus@gmail.com>
2013-02-06 10:44:11 -0500

    Added Crypto HAL

---
+++ src/hal/x86/HAL_crypt.c
@@ -0,0 +1,14 @@
+/***
+ *	Author:		Fogus
+ *	Website:	http://fogus.me/thunks/osdev.html
+ ***/
+
+#include <hal/crypt.h>
+#include <gadfly/kdefs.h>
+
+
+byte HAL_get_entropy( void )
+{
+  return 0;
+}
+
