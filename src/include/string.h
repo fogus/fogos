4d4d12391fe579aa75631b820d032b2386727b9b
fogus <mefogus@gmail.com>
2013-02-06 10:54:01 -0500

    strings

---
+++ src/include/string.h
@@ -0,0 +1,10 @@
+/***
+ *	Author:		Fogus
+ *	Website:	http://fogus.me/thunks/osdev.html
+ ***/
+
+#include <os.h>
+
+void *memset( void *p, int c, size_t n );
+void *memcpy( void *dst, const void *src, size_t n );
+
