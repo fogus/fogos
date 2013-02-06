ffec66b35d04ee1f522160ef9cec19d6bed45e93
fogus <mefogus@gmail.com>
2013-02-06 10:33:04 -0500

    Stage 2 x86 i32 bootloader

---
+++ src/boot/x86/stage2.asm
@@ -0,0 +1,14 @@
+;
+;   fogOS
+;   by
+;   Fogus
+;
+;   Filename:   stage2.asm
+;
+;   Pupose:     Eventually, this will be fancy pants and do some
+;		cool picture display or somesuch. 
+;
+;   Assemble using:
+;      nasm -f elf -o stage2.o stage2.asm
+;
+
